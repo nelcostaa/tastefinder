e6dcadc997efa2bbde44eb6ddccc2c4ecd9b2dd8
nelcostaa <nelcostaa@hotmail.com>
2025-04-24 18:56:10 -0300

    feat: script para extrair os dados da api do google places

---
+++ src/data/extract_google_places.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+import requests
+from dotenv import load_dotenv
+
+## OBTEM A API DE UM ARQUIVO DENTRO DE .env
+## ARQUIVO NÃO SUBIDO PARA GIT POR SEGURANÇÃ
+## OBTER API KEY NO GOOGLE CLOUD PLACES E SUBSTITUIR NA VAR API_KEY
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_PLACES_API_KEY")  ## TROCAR AQUI PELA SUA API_KEY
+location = "-25.4284,-49.2733"  ## LOCALIZAÇÃO DE CURITIBA
+
+radius = 5000  ## RAIO MÁXIMO PARA API CONSIDEREAR
+type_of_place = "restaurant"  ## TIPO DE ESTABELECIMENTO A SER CONSIDERADO
+
+params = {
+    "location": location,
+    "radius": radius,
+    "type": type_of_place,
+    "key": api_key,
+}
+
+response = requests.get(
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params
+)
+
+dados = response.json()
+restaurantes = dados.get("results", [])
+
+while dados.get("next_page_token"):
+    token = dados.get("next_page_token")
+    time.sleep(2)
+    params["pagetoken"] = token
+    response = requests.get(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params
+    )
+    dados = response.json()
+    restaurantes.extend(dados.get("results", []))
+
+for r in restaurantes:
+    print(r["name"])
+
+print(dados.get("next_page_token"))
