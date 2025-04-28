bca103ad27498aebafa5570a2b0fe385a791a061
nelcostaa <nelcostaa@hotmail.com>
2025-04-28 20:02:38 -0300

    refactor: wraps data request in functions

--- src/data/extract_google_places.py
+++ src/data/extract_google_places.py
@@ -1,6 +1,8 @@
 import os
 import time
+from pathlib import Path
 
+import pandas as pd
 import requests
 from dotenv import load_dotenv
 
@@ -9,42 +11,55 @@ from dotenv import load_dotenv
 ## OBTER API KEY NO GOOGLE CLOUD PLACES E SUBSTITUIR NA VAR API_KEY
 load_dotenv()
 
-api_key = os.getenv("GOOGLE_PLACES_API_KEY")  ## TROCAR AQUI PELA SUA API_KEY
-location = "-25.4284,-49.2733"  ## LOCALIZAÇÃO DE CURITIBA
 
-radius = 50_000  ## RAIO DA BUSCA EM METROS
-type_of_place = "restaurant"  ## TIPO DE ESTABELECIMENTO A SER CONSIDERADO
+def make_request(key: str, location: str, radius: int, type: str) -> object:
+    params = {
+        "location": location,
+        "radius": radius,
+        "type": type_of_place,
+        "key": api_key,
+    }
 
-params = {
-    "location": location,
-    "radius": radius,
-    "type": type_of_place,
-    "key": api_key,
-}
-
-response = requests.get(
-    "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params
-)
-
-dados = response.json()
-restaurantes = dados.get("results", [])
-
-while dados.get("next_page_token"):
-    ## CADA REQUEST RETORNA NO MÁXIMO 20 VALORES
-    ## E AO FINAL UM TOKEN PARA IR PARA PRÓXIMA PÁGINA
-    token = dados.get("next_page_token")
-    time.sleep(2)  ## GOOGLE PLACES PEDE UMA PEQUENA PAUSA ENTRE REQUESTS
-    params["pagetoken"] = token  ## ATUALIZA OS PARÂMETROS COM O TOKEN
-
-    ## REQUESTS ACESSANDO A PRÓXIMA PÁGINA
     response = requests.get(
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params
     )
+
     dados = response.json()
-    restaurantes.extend(dados.get("results", []))
+    restaurantes = dados.get("results", [])
+
+    if dados.get("next_page_token"):
+        get_next_page(dados, restaurantes, params)
+
+    return restaurantes
+
+
+def get_next_page(dados, restaurantes: list, params: dict):
+    while dados.get("next_page_token"):
+        ## CADA REQUEST RETORNA NO MÁXIMO 20 VALORES
+        ## E AO FINAL UM TOKEN PARA IR PARA PRÓXIMA PÁGINA
+        token = dados.get("next_page_token")
+        time.sleep(2)  ## GOOGLE PLACES PEDE UMA PEQUENA PAUSA ENTRE REQUESTS
+        params["pagetoken"] = token  ## ATUALIZA OS PARÂMETROS COM O TOKEN
+
+        ## REQUESTS ACESSANDO A PRÓXIMA PÁGINA
+        response = requests.get(
+            "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+            params=params,
+        )
+        dados = response.json()
+        restaurantes.extend(dados.get("results", []))
+
 
 for r in restaurantes:
     print(r["name"])
 
-print(len(restaurantes))
-print(dados.get("next_page_token"))
+data_path = Path("data")
+processed_data = data_path / "processed" / "curitiba_bairros_offsets_2025-05.csv"
+df = pd.read_csv(processed_data)
+
+api_key = os.getenv("GOOGLE_PLACES_API_KEY")  ## TROCAR AQUI PELA SUA API_KEY
+radius = 1500  ## RAIO DA BUSCA EM METROS
+type_of_place = "restaurant"  ## TIPO DE ESTABELECIMENTO A SER CONSIDERADO
+
+for location in df["coord_grouped"]:
+    pass
